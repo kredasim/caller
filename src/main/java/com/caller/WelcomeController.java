f44748973c42215b8f422dfc48f363cfefb03229
SKR <simeon.kredatus@gmail.com>
2014-01-01 19:16:48 +0100

    spring infrastructure

---
+++ src/main/java/com/caller/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.caller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/welcome")
+public class WelcomeController {
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String getHome() {
+		return "home";
+	}
+
+}
